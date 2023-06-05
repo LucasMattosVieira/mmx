cb884ec6157d4e91a57ed320fd5bbd269065bc22
Lucas Marçal Coutinho <56159095+lucasmc64@users.noreply.github.com>
2023-06-05 15:57:03 -0300

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,10 @@
 function handleSubmit(event) {
-
+    const form = document.forms[0];
+    
+    if(form.email.trim() === "" || form.password.trim() === "") {
+        event.preventDefault();
+        console.error("Há campos obrigatórios não preenchidos.");
+    }
 }
 
 document.getElementById("loginForm").addEventListener("submit", handleSubmit);
